64b8d715d48352491067e7133cc8473e281b7194
Jessica Bowmaster <jessica_bowmaster@outlook.com>
2024-03-06 23:57:57 -0500

    Use props to pass attribute from parent component to child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,84 @@
-//import * as React from 'react';
+import PropTypes from 'prop-types'
 
-// const array = [0, 5, 10, 22];
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
-// function renderArrayValue(val) {
-//   <div>{val}</div>
-// }
+  return(
+  <div>
+    <h1>My Hacker Stories</h1>
 
-//{array.map(renderArrayValue)}
+    <Search />
 
-function App() {
+    <hr />
+
+    <List list={stories}/>
+  </div>
+  );
+};
+
+const Search = () => {
+  const handleChange = (event) => {
+    // synthetic event
+    console.log(event);
+    // value of target (here: input HTML element)
+    console.log(event.target.value);
+  };
+
   return (
     <div>
-      <h1>Hello React</h1>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text"></input>
+      <input id="search" type="text" onChange={handleChange} />
     </div>
   );
+};
+
+const List = (props) => (
+  <ul>
+    {props.list.map((item) => (
+      <Item key={item.objectID} item={item}></Item>
+    ))}
+  </ul>
+);
+
+const Item = (props) => {
+  const item = props.item;
+  return(
+    <li>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </li>
+  )
 }
 
-export default App
+List.propTypes = {
+  list: PropTypes.array
+}
+
+Item.propTypes = {
+  item: PropTypes.any,
+  key: PropTypes.number
+
+}
+
+export default App;
